3d16e33bb3c22be203000b2ca58637445ff9a548
qq <gh818.zhou@gmail.com>
2026-04-24 22:14:50 +0800

    docs: 更新内容

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,4 +10,21 @@
     toggle.setAttribute("aria-expanded", expanded ? "false" : "true");
     header.classList.toggle("nav-open", !expanded);
   });
+
+
+  // Google Analytics gtag.js
+  var GA_ID = 'G-T5PPQX5BDS';
+
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=' + GA_ID;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function () {
+    window.dataLayer.push(arguments);
+  };
+
+  window.gtag('js', new Date());
+  window.gtag('config', GA_ID);
 })();
